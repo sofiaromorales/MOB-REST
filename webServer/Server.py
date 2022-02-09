22c5f73d34b25e55664c0589bba043cb5cd89d89
Sofia <sofiaromorales@gmail.com>
2022-02-09 14:11:35 -0400

    feat - CREATE, DELETE, GET requests

--- webServer/Server.py
+++ webServer/Server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import List
 from uuid import UUID, uuid4
 from datetime import datetime
@@ -24,11 +24,32 @@ objects: List[Object] = [
 async def fetch_objects():
     return objects;
 
+# GET object by name
+@app.get('/api/objects/{object_name}')
+async def fetch_object(object_name: str):
+    for object in objects:
+        if object.name == object_name:
+            return object
+    raise HTTPException(status_code=404, detail='Objet Not Found')
+
 # POST create new object
 @app.post('/api/objects')
 async def create_object(object: Object):
-    #TODO: Look for repeated object and if exists return code 409
-    #TODO: if name has blank space turn it into skewer-case
+    # If name has blank space turn it into skewer-case
+    object.name = object.name.replace(' ', '-')
+    # Look for repeated object and if exists return code 409
+    for obj in objects:
+        if object.name == obj.name:
+            raise HTTPException(status_code=409, detail='Object Already Exists')
     object.date_created = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
     objects.append(object)
     return { 'name' : object.name }
+
+# DELETE object by name
+@app.delete('/api/objects/{object_name}')
+async def fetch_object(object_name: str):
+    for object in objects:
+        if object.name == object_name:
+            objects.remove(object)
+            return { 'name' : object_name }
+    raise HTTPException(status_code=404, detail='Object Not Found')
