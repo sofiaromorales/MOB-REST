c9571bba74449411e0409142d289dbb95a3a2f5a
Sofia <sofiaromorales@gmail.com>
2022-02-10 21:05:00 -0400

    fix

--- coordinator/Server.py
+++ coordinator/Server.py
@@ -12,6 +12,7 @@ sys.path.insert(0, parentdir)
 import constants
 
 def replicateObjects(vote_request, data):
+    print('coordinator replicateObjects')
     ##  Call replication servers method `replicateObject(vote_request, data)`
     if vote_request == 'randomize':
         vote = 'commit' if bool(random.getrandbits(1)) else 'abort'
@@ -26,9 +27,9 @@ def restoregetObjects(data):
 
 # restoreServerProxy = xmlrpc.client.ServerProxy('http://' + constants.COORDINATOR_ADDRESS + ':' + str(constants.RESTORE_SERVER_1_PORT))
 
-replicationServerProxy = xmlrpc.client.ServerProxy('http://' + constants.COORDINATOR_ADDRESS + ':' + str(constants.REPLICATION_SERVER_1_PORT))
+replicationServerProxy = xmlrpc.client.ServerProxy('http://' + constants.REPLICATION_SERVER_1_ADDRESS + ':' + str(constants.REPLICATION_SERVER_1_PORT))
 
-server = SimpleXMLRPCServer((constants.REPLICATION_SERVER_1_ADDRESS, constants.COORDINATOR_PORT), allow_none=True)
+server = SimpleXMLRPCServer((constants.COORDINATOR_ADDRESS, constants.COORDINATOR_PORT), allow_none=True)
 server.register_function(restoregetObjects, 'restoregetObjects')
 server.register_function(replicateObjects, 'replicateObjects')
 # server = SimpleXMLRPCServer((constants.REPLICATION_SERVER_1_ADDRESS, constants.COORDINATOR_PORT), allow_none=True)
