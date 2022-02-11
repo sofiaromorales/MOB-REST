1bf2990403850132fc6f1dbaaa1a3ae1fa7c2949
gabrieltagliafico <75792169+gabrieltagliafico@users.noreply.github.com>
2022-02-10 20:00:24 -0400

    Update server.py

--- replicationServer/server.py
+++ replicationServer/server.py
@@ -2,8 +2,6 @@ from xmlrpc.server import SimpleXMLRPCServer
 import os
 import sys
 import inspect
-import json
-
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
@@ -14,12 +12,19 @@ import constants
 def replicateRequest(vote_request, data):
     print('replicateRequest')
     if vote_request == 'commit':
-        with open('data.json', 'w') as fp:
-                json.dump(data, fp,sort_keys=True, indent=4)
+        # TODO: Implement replication service
         return 1
     else:
         return -1
 
+def restoreRequest( data):
+    print('restoreRequest')
+    # TODO: Implement replication service
+
+
 server = SimpleXMLRPCServer((constants.REPLICATION_SERVER_1_ADDRESS, constants.REPLICATION_SERVER_1_PORT), allow_none=True)
+serverrest = SimpleXMLRPCServer((constants.RESTORE_SERVER_1_ADDRESS, constants.RESTORE_SERVER_1_PORT), allow_none=True)
+
+serverrest.register_function(restoreRequest, 'restoreRequest')
 server.register_function(replicateRequest, 'replicateRequest')
 server.serve_forever()
